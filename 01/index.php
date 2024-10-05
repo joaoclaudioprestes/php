d9e86db4a3162cb2e1ece5127fbafb9d793192b9
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-10-04 23:40:03 -0300

    Create index.php

---
+++ 01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primeira - Aula</title>
+</head>
+
+<body>
+  <?php
+  $start = 'Olá, Mundo!';
+  echo $start;
+  echo '<br>';
+  echo '<br>';
+  echo 'Aula de PHP';
+  ?>
+</body>
+
+</html>
