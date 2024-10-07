4e7a36d01f6db8d365af8466f1b49c890481ed27
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-10-06 21:02:36 -0300

    :tada: iniciando projeto com arquitetura MVC/PHP.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Controller\Pages\Home;
+
+
+// :: - Accessing a static method
+echo Home::getHome();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jprestes/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd9e86db4a3162cb2e1ece5127fbafb9d793192b9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jprestes/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd9e86db4a3162cb2e1ece5127fbafb9d793192b9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
